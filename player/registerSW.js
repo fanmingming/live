4a41cba18e1f6e824aab27f3e85278d66e5f36d4
范明明 <mingming@live.com>
2022-11-23 18:50:04 +0800

    Update registerSW.js

--- player/registerSW.js
+++ player/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/player/sw.js', { scope: '/player/' })})}
