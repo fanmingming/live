36f8cad5f9b422a4846d2f04dad8b60b6ba565ca
范明明 <mingming@live.com>
2022-11-23 18:58:59 +0800

    Add files via upload

---
+++ player/player.js
@@ -0,0 +1,18 @@
+  var video = document.getElementById('video');
+  var vurl = location.search.split('vurl=')[1];
+
+
+  var videoSrc = vurl;
+  //
+  // First check for native browser HLS support
+  //
+  if (video.canPlayType('application/vnd.apple.mpegurl')) {
+    video.src = videoSrc;
+    //
+    // If no native HLS support, check if HLS.js is supported
+    //
+  } else if (Hls.isSupported()) {
+    var hls = new Hls();
+    hls.loadSource(videoSrc);
+    hls.attachMedia(video);
+  }
