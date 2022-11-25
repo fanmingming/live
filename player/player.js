44ad1a9b69b8826b46eeb5467a7b715e1d2d4875
范明明 <mingming@live.com>
2022-11-25 19:28:45 +0800

    规范了代码，添加了注释。

--- player/player.js
+++ player/player.js
@@ -4,7 +4,7 @@
   // 检查浏览器是否支持HLS
   if (video.canPlayType('application/vnd.apple.mpegurl')) {
     video.src = videoSrc;
-    // 如不支持则调用HLS.js
+    // 如不支持则调用hls.min.js
   } else if (Hls.isSupported()) {
     var hls = new Hls();
     hls.loadSource(videoSrc);
