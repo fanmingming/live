67003f3f1d585e6a4ba28f78ce17639a8cc34880
范明明 <mingming@live.com>
2022-11-25 19:32:10 +0800

    规范了代码，添加了注释。

--- player/player.js
+++ player/player.js
@@ -1,10 +1,10 @@
   var video = document.getElementById('video');
   var vurl = location.search.split('vurl=')[1];
   var videoSrc = vurl;
-  // 检查浏览器是否支持HLS
+  // 检查浏览器是否支持hls
   if (video.canPlayType('application/vnd.apple.mpegurl')) {
     video.src = videoSrc;
-    // 如不支持则调用hls.min.js
+  // 如不支持则调用hls.min.js
   } else if (Hls.isSupported()) {
     var hls = new Hls();
     hls.loadSource(videoSrc);
